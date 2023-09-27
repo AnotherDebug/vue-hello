d01723d59d02081c6fb85904dd4891dcdb2ec691
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-27 16:36:21 +0200

    [ADD]: image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 const { createApp } = Vue;
 
+
 createApp ({
     data() {
        return{
-        message : "Ciao, questo è il tuo primo messaggio su Vue!"
+        message : "Ciao, questo è il tuo primo messaggio su Vue!",
+        image : "https://picsum.photos/200"
        }
     }
+  
 }).mount("#app");
